9457637a1d0d395bdf8156ae6265a911534f92f1
Damyres <damyresmaciell@gmail.com>
2025-05-23 15:24:55 -0300

    Crair compoenet usuário utilizando variáveis do javaScript

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import { Botao } from "../Botao/Botao"
+import Usuario from "../Usuario/Usuario"
 
 export const Home = () => {
 
     return (
         <>
-            <h1>Oi</h1>
+            <h1>Bem vindo ao meu aplicativo</h1>
             <Botao />
+            <Usuario/>
         </>
     )
 }
---
+++ src/components/Usuario/Usuario.tsx
@@ -0,0 +1,14 @@
+const user = {
+    name: 'Merlyn'
+};
+
+export default function Usuario(){
+    return(
+        <>
+        <h1>{user.name}</h1>
+        <p>Sou um componente usuário para exibir o nome utilizando variáveis</p>
+        </>
+    )
+
+}
+
